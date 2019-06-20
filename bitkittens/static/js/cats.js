4d6b0731e2a40fa56eced5a892985363ec9e77f1
Brody Currie <brodycurrie@gmail.com>
2019-06-20 17:36:21 -0400

    Completed assignment

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -1,2 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
+  const catButton = document.querySelector('.summon-cats');
+  
+  catButton.addEventListener('click', () => {
+    axios.get("http://bitkittens.herokuapp.com/cats.json", {
+      params: {number: 5},
+    })
+    .then((response) => {
+      const catList = response.data.cats;
+      console.log(catList)
+      
+      catList.forEach((cat, index) => {
+        let dataImg = document.createElement('img');
+        dataImg.src = cat.photo;
+        dataImg.alt = `Photo of: ${cat.name}`;
+        const catBoxes = document.querySelectorAll('.cat-box');
+        catBoxes[index].append(dataImg);
+      })
+    })
+      .catch((error) => {
+        console.log(error);
+    })
+    // console.log('hi')
+  });
 });
