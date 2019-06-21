52cec54a391abc001a998e913071e4486d7cb530
Brody Currie <brodycurrie@gmail.com>
2019-06-21 17:00:20 -0400

    fixed my javascript loading twice

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -1,30 +1,41 @@
+window.load_counter = 0
+
 document.addEventListener("DOMContentLoaded", function() {
+  window.load_counter++
+
+  console.log(`I AM MULTIPLE MAN, called ${window.load_counter} times.`);
+
+  let cat_counter = document.querySelector("#cat_counter")
+  cat_counter.innerText = window.load_counter;
+
   const catButton = document.querySelector('.summon-cats');
-  
   catButton.addEventListener('click', () => {
     axios.get("http://bitkittens.herokuapp.com/cats.json", {
-      params: {number: 5},
+      params: {
+        number: 5
+      }
     })
     .then((response) => {
+      console.log("I'm doubled!");
       const catList = response.data.cats;
-      
+
       catList.forEach((cat, index) => {
         let dataImg = document.createElement('img');
         dataImg.src = cat.photo;
         dataImg.alt = `Photo of: ${cat.name}`;
+
         const catBoxes = document.querySelectorAll('.cat-box');
         catBoxes[index].append(dataImg);
-        
+
         let bookEntry = document.createElement('li');
-        // bookEntry.innerText = cat.name;
         bookEntry.innerText = cat.name
         const catBook = document.querySelector('#cat-book');
 
         catBook.append(bookEntry);
       });
     })
-      .catch((error) => {
-        console.log(error);
-    })
+    .catch((error) => {
+      console.log(error);
+    });
   });
 });
